c7b47c8ac7fec84f2b1b4b3c2f25929991c9bbba
Sebastian Anklamm <sanklamm@gmail.com>
2022-12-01 09:28:57 +0100

    day 1

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -1,3 +1,34 @@
-;; # 🎄 Advent of Clerk: Day 1
+;; # 🎄 Advent of Code: Day 1
 (ns advent-of-clerk.day-01
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [util.utils :as utils]))
+
+(def day 1)
+
+(utils/get-input-data day)
+
+(def data (str "input-" day ".txt"))
+
+(defn parse [lines]
+  (->> lines
+       (partition-by empty?)
+       (remove (comp empty? first))
+       (map #(map parse-long %))))
+
+;; part 1
+(->> (utils/read-lines data)
+     parse
+     (map #(reduce + %))
+     (apply max))
+;; => 70116
+
+
+;; part 2
+(->> (utils/read-lines data)
+     parse
+     (map #(reduce + %))
+     sort
+     reverse
+     (take 3)
+     (reduce +))
+;; => 206582
